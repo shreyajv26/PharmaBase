71d8be59442f7ce66606449e853e4bf83accebec
Shreya Pampattiwar <shreyajv26@gmail.com>
2021-04-22 16:04:34 -0400

    Added Integration with AWS Database

--- pharmaBase/pharmaBase/src/test/java/com/accendero/pharmaBase/WebAppControllerTest.java
+++ pharmaBase/pharmaBase/src/test/java/com/accendero/pharmaBase/WebAppControllerTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JsonParseException;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
